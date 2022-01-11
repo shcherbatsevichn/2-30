8fb6f978af09d7967077cd467c045f34f87c23d5
shcherbatsevichn <shcherbatsevich.n.dvl@yandex.by>
2022-01-11 18:02:41 +0300

    2-30-1

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php error_reporting(-1);
 // Вычислить  значение  многочлена  и  его  первой  производной  
 //в  заданной точке x (коэффициенты хранятся в массивах).  
-$A = [4, 2, 1, 4];
-$x =7;
-
-$i = count($A)-1;
+$A = [3, 7, 9, 5];
+$x =2;
 for($n = 0; $n <count($A); $n++){
-    $res += $A[$n]*($x ** $i);
-    $i--;
-    
+    $res += $A[$n]*($x ** $n);
 }
+$fstpr = get_pr($A, $x);
+
 echo("Значение многочлена".$res."<br>"); 
-$fstpr = $res % $x;
 echo("Его первая производная".$fstpr);
+
+function get_pr($array, $x){
+    $arr =[];
+    $n = 0;
+    for($i = 1; $i < count($array); $i++){
+        $arr[$n] = $array[$i];
+        $n++;
+    }
+    for($i = 0; $i < count($array); $i++){
+        $arr[$i] = $arr[$i] *($i+1);
+    }
+    $res = 0;
+    for($n = 0; $n < count($arr); $n++){
+        $res += $arr[$n]*($x ** $n);
+    }
+    return $res;
+}
+
+
 
